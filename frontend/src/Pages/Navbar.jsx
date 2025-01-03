4c3654a1f8a7340ff977b121afa0af6efb89baf7
srsohamvirani <soham.virani@swiftrut.com>
2025-01-04 04:06:55 +0530

    Add create Folder

---
+++ frontend/src/Pages/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline">
+      Hello world!
+    </p>
+    </div>
+  )
+}
+
+export default Navbar
