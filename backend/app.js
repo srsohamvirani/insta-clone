f85282890b47963af93581e5791bf5c61e75e97d
srsohamvirani <soham.virani@swiftrut.com>
2025-01-05 00:56:45 +0530

    mongodb connect

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
-const cors = require("cors");
-const data = require("./data")
+const mongoose = require("mongoose");
+const mongoUrl = require("./keys");
 
 
-app.use(cors());
+mongoose.connect(mongoUrl);
 
-app.get("/about", (req, res) => {
-    res.json("about page");
+mongoose.connection.on("connected", () => {
+    console.log("successfully Connected to database");
 });
 
-app.get("/", (req, res) => {
-    res.json(data);
+mongoose.connection.on("error", () => {
+    console.log("not connected to mongodb");
 });
 
 app.listen(PORT, () => {
---
+++ backend/keys.js
@@ -0,0 +1,2 @@
+const mongoUrl = 'mongodb+srv://sohamvirani:Soham123@cluster0.4zclm.mongodb.net/instaclone';
+module.exports =  mongoUrl ;
